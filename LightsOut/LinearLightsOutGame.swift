61e8607e168894296884e972f40ac79a8771c4ff
Kiana Caston <castonkr@rose-hulman.edu>
2018-03-22 11:04:14 -0400

    finished lights out with swift!

--- LightsOut/LinearLightsOutGame.swift
+++ LightsOut/LinearLightsOutGame.swift
@@ -7,3 +7,71 @@
 //
 
 import Foundation
+class LinearLightsOutGame: CustomStringConvertible {
+    var numLights: Int
+    var movesTaken: Int
+    var gameState: String
+    var lightStates: [Bool]
+    
+    init(numLights: Int) {
+        self.gameState = "setup"
+        self.numLights = numLights
+        self.movesTaken = 0
+        self.lightStates = [Bool](repeating: false, count: numLights)
+        randomize()
+    }
+    
+    func randomize() {
+        for _ in 0..<100 {
+            let randomNumber : Int = Int(arc4random_uniform(UInt32(numLights)))
+            pressedLightAtIndex(randomNumber)
+        }
+        gameState = "play"
+    }
+    
+    func pressedLightAtIndex(_ index: Int) -> Bool{
+        if (checkWin() && gameState != "setup"){
+            return true
+        }
+
+        if (index > 0) {
+            lightStates[index - 1] = !lightStates[index - 1]
+        }
+        if (index < numLights - 1) {
+            lightStates[index + 1] = !lightStates[index + 1]
+        }
+        lightStates[index] = !lightStates[index]
+        
+        if (gameState != "setup"){
+            movesTaken += 1
+        }
+        return checkWin();
+    }
+    
+    func checkWin() -> Bool{
+        for index in lightStates {
+            if (index) {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func getGameString() -> String{
+        var gameString = ""
+        for index in lightStates {
+            if (index){
+                gameString += "1"
+            }
+            else {
+                gameString += "0"
+            }
+        }
+        return gameString
+    }
+    
+    var description: String {
+        return "Lights: \(getGameString()) Moves: \(movesTaken)"
+    }
+}
+
--- LightsOut/ViewController.swift
+++ LightsOut/ViewController.swift
@@ -9,17 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var lightButtons: [UIButton]!
+    @IBOutlet weak var gameStateLabel: UILabel!
+    @IBOutlet weak var gameStateNavBar: UINavigationBar!
+    
+    var game = LinearLightsOutGame(numLights: 13)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        gameStateNavBar.titleTextAttributes = [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 20)]
+        updateView()
     }
 
 
+    @IBAction func pressedNewGame(_ sender: Any) {
+        game = LinearLightsOutGame(numLights: 13)
+        updateView()
+    }
+    
+    @IBAction func lightButtonPressed(_ sender: Any) {
+        let lightButton = sender as! UIButton
+        game.pressedLightAtIndex(lightButton.tag)
+        updateView()
+    }
+    
+    func updateView() {
+        let on = #imageLiteral(resourceName: "light_on")
+        let off = #imageLiteral(resourceName: "light_off")
+        let lightStates = game.lightStates
+        print(game.checkWin())
+        
+      
+        
+        // iPhone
+        if (traitCollection.horizontalSizeClass == UIUserInterfaceSizeClass.compact) {
+            gameStateLabel.text = "Moves: \(game.movesTaken)"
+        } else {
+            gameStateNavBar.topItem?.title = "Moves: \(game.movesTaken)"
+        }
+        
+        for i in 0..<lightStates.count {
+            let button = lightButtons[i]
+            if (lightStates[i]) {
+                button.setBackgroundImage(on, for: UIControlState.normal)
+            } else {
+                button.setBackgroundImage(off, for: UIControlState.normal)
+            }
+        }
+    }
 }
 
